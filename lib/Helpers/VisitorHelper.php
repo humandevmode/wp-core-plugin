0d5f00679a0831dce5591ba04c88b1249039edba
humandevmode <humandevmode@gmail.com>
2018-01-12 10:56:24 +0300

    Add mobile detect
    Add mobile detect

---
+++ lib/Helpers/VisitorHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Core\Helpers;
+
+use Detection\MobileDetect;
+
+class VisitorHelper {
+	protected static $detect;
+
+	public static function isFromSE() {
+		$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
+		if (preg_match('/^https?:\/\//', $referer)) {
+			$host = parse_url($referer, PHP_URL_HOST);
+			if ($host and preg_match('/(yandex|google|mail\.ru|rambler\.ru)/', $host)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static function isMobile() {
+		return static::getMobileDetect()->isMobile();
+	}
+
+	public static function isTablet() {
+		return static::getMobileDetect()->isTablet();
+	}
+
+	public static function isAndroid() {
+		return static::getMobileDetect()->isAndroidOS();
+	}
+
+	public static function isIOS() {
+		return static::getMobileDetect()->isiOS();
+	}
+
+	/**
+	 * @return MobileDetect
+	 */
+	protected static function getMobileDetect() {
+		if (!isset(static::$detect)) {
+			static::$detect = new MobileDetect();
+		}
+
+		return static::$detect;
+	}
+}
